0d1147c383eaec0385198b060a923716582d137a
Ian Webster <typppo@gmail.com>
2015-05-22 09:51:50 -0700

    fix preloading and remove tabs

--- js/main.js
+++ js/main.js
@@ -1,71 +1,70 @@
 (function () {
-	var webglEl = document.getElementById('webgl');
+  var webglEl = document.getElementById('webgl');
 
-	if (!Detector.webgl) {
-		Detector.addGetWebGLMessage(webglEl);
-		return;
-	}
+  if (!Detector.webgl) {
+    Detector.addGetWebGLMessage(webglEl);
+    return;
+  }
 
-	var width  = window.innerWidth,
-		height = window.innerHeight;
+  var width  = window.innerWidth,
+    height = window.innerHeight;
 
-	// Earth params
-	var radius   = 0.5,
-		segments = 32,
-		rotation = 11;
+  // Earth params
+  var radius   = 0.5,
+    segments = 32,
+    rotation = 11;
 
-	var scene = new THREE.Scene();
+  var scene = new THREE.Scene();
 
-	var camera = new THREE.PerspectiveCamera(45, width / height, 0.01, 1000);
-	camera.position.z = 4;
+  var camera = new THREE.PerspectiveCamera(45, width / height, 0.01, 1000);
+  camera.position.z = 4;
 
-	var renderer = new THREE.WebGLRenderer();
-	renderer.setSize(width, height);
+  var renderer = new THREE.WebGLRenderer();
+  renderer.setSize(width, height);
 
-	//scene.add(new THREE.AmbientLight(0x333333));
-	scene.add(new THREE.AmbientLight(0x666666));
-	//scene.add(new THREE.AmbientLight(0xffffff));
+  //scene.add(new THREE.AmbientLight(0x333333));
+  scene.add(new THREE.AmbientLight(0x666666));
+  //scene.add(new THREE.AmbientLight(0xffffff));
 
-	var light = new THREE.DirectionalLight(0xffffff, 1);
-	light.position.set(5,3,5);
-	scene.add(light);
+  var light = new THREE.DirectionalLight(0xffffff, 1);
+  light.position.set(5,3,5);
+  scene.add(light);
 
   var sphere = createSphere(radius, segments, imagePathForYearsAgo(600));
-	sphere.rotation.y = rotation;
-	scene.add(sphere)
+  sphere.rotation.y = rotation;
+  scene.add(sphere)
 
   var clouds = createClouds(radius, segments);
-	clouds.rotation.y = rotation;
-	scene.add(clouds)
+  clouds.rotation.y = rotation;
+  scene.add(clouds)
 
-	var stars = createStars(90, 64);
-	scene.add(stars);
+  var stars = createStars(90, 64);
+  scene.add(stars);
 
-	var controls = new THREE.TrackballControls(camera, webglEl);
+  var controls = new THREE.TrackballControls(camera, webglEl);
 
-	webglEl.appendChild(renderer.domElement);
+  webglEl.appendChild(renderer.domElement);
 
   setupSelect();
 
-	render();
+  render();
 
   preloadTextures();
 
-	function render() {
-		controls.update();
-		//sphere.rotation.y += 0.0005;
-		sphere.rotation.y += 0.001;
-		//clouds.rotation.y += 0.0005;
-		clouds.rotation.y += 0.001;
-		requestAnimationFrame(render);
-		renderer.render(scene, camera);
-	}
+  function render() {
+    controls.update();
+    //sphere.rotation.y += 0.0005;
+    sphere.rotation.y += 0.001;
+    //clouds.rotation.y += 0.0005;
+    clouds.rotation.y += 0.001;
+    requestAnimationFrame(render);
+    renderer.render(scene, camera);
+  }
 
   function preloadTextures() {
     for (key in EXPLAIN_MAP) {
       var i = new Image()
-      i.src = 'images/scrape/' + key + 'Marect.jpg';
-      console.log('preloaded', i.src);
+      i.src = imagePathForYearsAgo(key);
     }
   }
 
@@ -107,49 +106,49 @@
         + ((years+'').length < 3 ? '0' + years : years) + 'Marect.jpg';
   }
 
-	function createSphere(radius, segments, img) {
+  function createSphere(radius, segments, img) {
     console.log('Creating sphere foe', img);
     //var map = THREE.ImageUtils.loadTexture('images/early_jurassic.jpg');
     //var map = THREE.ImageUtils.loadTexture('images/late_cretaceous.jpg');
     var map = THREE.ImageUtils.loadTexture(img);
-		var mesh = new THREE.Mesh(
-			new THREE.SphereGeometry(radius, segments, segments),
-			new THREE.MeshPhongMaterial({
-				//map:         THREE.ImageUtils.loadTexture('images/2_no_clouds_4k.jpg'),
-				map:         map,
+    var mesh = new THREE.Mesh(
+      new THREE.SphereGeometry(radius, segments, segments),
+      new THREE.MeshPhongMaterial({
+        //map:         THREE.ImageUtils.loadTexture('images/2_no_clouds_4k.jpg'),
+        map:         map,
         "color": 0xbbbbbb, "specular": 0x111111, "shininess": 1,
-				bumpMap:     map,
-				bumpScale:   0.02,
-				specularMap: map,
-				//specular:    new THREE.Color('grey')
+        bumpMap:     map,
+        bumpScale:   0.02,
+        specularMap: map,
+        //specular:    new THREE.Color('grey')
         /*
-				bumpMap:     THREE.ImageUtils.loadTexture('images/elev_bump_4k.jpg'),
-				bumpScale:   0.005,
-				specularMap: THREE.ImageUtils.loadTexture('images/water_4k.png'),
-				specular:    new THREE.Color('grey')
+        bumpMap:     THREE.ImageUtils.loadTexture('images/elev_bump_4k.jpg'),
+        bumpScale:   0.005,
+        specularMap: THREE.ImageUtils.loadTexture('images/water_4k.png'),
+        specular:    new THREE.Color('grey')
         */
-			})
-		);
+      })
+    );
     return mesh;
-	}
+  }
 
-	function createClouds(radius, segments) {
-		return new THREE.Mesh(
-			new THREE.SphereGeometry(radius + 0.003, segments, segments),
-			new THREE.MeshPhongMaterial({
-				map:         THREE.ImageUtils.loadTexture('images/fair_clouds_4k.png'),
-				transparent: true
-			})
-		);
-	}
+  function createClouds(radius, segments) {
+    return new THREE.Mesh(
+      new THREE.SphereGeometry(radius + 0.003, segments, segments),
+      new THREE.MeshPhongMaterial({
+        map:         THREE.ImageUtils.loadTexture('images/fair_clouds_4k.png'),
+        transparent: true
+      })
+    );
+  }
 
-	function createStars(radius, segments) {
-		return new THREE.Mesh(
-			new THREE.SphereGeometry(radius, segments, segments),
-			new THREE.MeshBasicMaterial({
-				map:  THREE.ImageUtils.loadTexture('images/galaxy_starfield.png'),
-				side: THREE.BackSide
-			})
-		);
-	}
+  function createStars(radius, segments) {
+    return new THREE.Mesh(
+      new THREE.SphereGeometry(radius, segments, segments),
+      new THREE.MeshBasicMaterial({
+        map:  THREE.ImageUtils.loadTexture('images/galaxy_starfield.png'),
+        side: THREE.BackSide
+      })
+    );
+  }
 }());
